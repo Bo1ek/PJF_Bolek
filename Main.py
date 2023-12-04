d719bce56a211642acec98b4d64b63f12dda9995
Jakub Bolek <98595825+Bo1ek@users.noreply.github.com>
2023-12-04 22:10:48 +0100

    Start of Django Project
    Django Project

--- Main.py
+++ Main.py
@@ -2,6 +2,9 @@ import django
 import pandas as pd
 import matplotlib as mat
 import sqlparse as sql
+import numpy as np
+from PIL import *
+import tkinter as tk
 print(django.get_version())
 print(pd.__version__)
 print(mat.__version__)
