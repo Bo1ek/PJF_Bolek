ae87573e25b7386aff89db1a8c942dbbdfc142a1
Jakub Bolek <jakub.bolek02@gmail.com>
2023-12-04 20:38:16 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,8 @@
 import django
 import pandas as pd
 import matplotlib as mat
+import sqlparse as sql
 print(django.get_version())
 print(pd.__version__)
 print(mat.__version__)
+print(sql.__version__)
